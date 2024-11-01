a85ee6c799b3ee0ca1d414fe216db6eec5afeb99
Heng Li <github@hengli.me>
2024-11-01 14:32:43 +0800

    adding cutsom

---
+++ ubuntu-desktop/docker_config/config_jupyterlab.sh
@@ -0,0 +1,6 @@
+pip install --no-cache-dir --upgrade \
+      jupyterlab \
+      ipywidgets \
+      matplotlib \
+      jupyterlab_language_pack_zh_CN \
+      -i https://mirrors.aliyun.com/pypi/simple
